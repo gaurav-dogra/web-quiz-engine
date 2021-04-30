f545ea70ae81e3dbed4368fa515e83bdc38d5ed1
Gaurav Dogra <gauru.dogra@gmail.com>
2021-04-30 02:46:36 +0100

    ServerResponseQuiz take Quiz in the constructor and convert it with id and without answer field

--- engine/ServerResponseQuiz.java
+++ engine/ServerResponseQuiz.java
@@ -1,2 +1,56 @@
-package engine;public class ServerResponseQuiz {
+package engine;
+
+import engine.model.Quiz;
+
+import java.util.List;
+
+public class ServerResponseQuiz {
+    private long id;
+    private String title;
+    private String text;
+    private String[] options;
+
+
+    public ServerResponseQuiz(List<Quiz> quizzes) {
+        Quiz quiz = quizzes.get(quizzes.size() - 1);
+        this.id = quizzes.size();
+        this.title = quiz.getTitle();
+        this.text = quiz.getTitle();
+        this.options = quiz.getOptions();
+    }
+
+    public ServerResponseQuiz() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
 }
