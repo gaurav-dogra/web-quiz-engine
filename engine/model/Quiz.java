3b6956f4baf5810b7fc59b5413325cd9b708c41e
Gaurav Dogra <gauru.dogra@gmail.com>
2021-04-30 03:08:24 +0100

    Quiz has a id field now

--- engine/model/Quiz.java
+++ engine/model/Quiz.java
@@ -1,12 +1,30 @@
 package engine.model;
 
 public class Quiz {
+
+    private long id;
     private String title;
     private String text;
     private String[] options;
     private int answer;
 
     public Quiz() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
     }
 
     public String getTitle() {
