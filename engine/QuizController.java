9cec0d75745488f9f04fca2f95906cf76fa3d837
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-01 10:26:56 +0100

    added method for endpoint /api/quizzes/{id}/solve in QuizController

--- engine/QuizController.java
+++ engine/QuizController.java
@@ -13,22 +13,35 @@ public class QuizController {
 
     @PostMapping("/api/quizzes")
     public ServerResponseQuiz createQuiz(@RequestBody Quiz quiz) {
+        System.out.println("QuizController.createQuiz");
         return quizService.save(quiz);
     }
 
     @GetMapping("/api/quizzes/{id}")
     public ServerResponseQuiz getQuizById(@PathVariable long id) throws NotFoundException {
-        ServerResponseQuiz serverResponseQuiz = quizService.getQuizById(id);
-        if (serverResponseQuiz == null) {
-            throw new NotFoundException(id + " Quiz does not exist");
+        System.out.println("QuizController.getQuizById");
+        if (quizService.quizExist(id)) {
+            return quizService.getQuizById(id);
         } else {
-            return serverResponseQuiz;
+            throw new NotFoundException(id + " Quiz does not exist");
         }
     }
 
     @GetMapping("/api/quizzes")
     public List<ServerResponseQuiz> getAll() {
+        System.out.println("QuizController.getAll");
         return quizService.getAll();
     }
 
+    @PostMapping("/api/quizzes/{id}/solve")
+    public Reply solve(@PathVariable long id, @RequestParam int answer) throws NotFoundException {
+        System.out.println("QuizController.solve");
+        System.out.println("id = " + id);
+        System.out.println("answer = " + answer);
+        if (quizService.quizExist(id)) {
+            return quizService.solve(id, answer);
+        } else {
+            throw new NotFoundException(id + " Quiz does not exist");
+        }
+    }
 }
