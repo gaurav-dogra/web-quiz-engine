7ea789e2de25dac39e1d63c6dd8351ec6ef24d87
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-01 13:44:22 +0100

    Added AnswerParameter for post request parameter answer

---
+++ engine/AnswerParameter.java
@@ -0,0 +1,16 @@
+package engine;
+
+public class AnswerParameter {
+    private int[] answer;
+
+    public AnswerParameter() {
+    }
+
+    public int[] getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int[] answer) {
+        this.answer = answer;
+    }
+}
