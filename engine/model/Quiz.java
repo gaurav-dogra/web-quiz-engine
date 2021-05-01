4a88a6768801e53c324d2df14ee0c01d62da90d5
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-01 13:19:36 +0100

    added Validation constraints on fields, and answer filed is now an int[]

--- engine/model/Quiz.java
+++ engine/model/Quiz.java
@@ -1,13 +1,21 @@
 package engine.model;
 
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Arrays;
 
+@Validated
 public class Quiz {
     private long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String text;
+    @Size(min = 2)
     private String[] options;
-    private int answer;
+    private int[] answer;
 
     public Quiz() {
     }
@@ -20,11 +28,11 @@ public class Quiz {
         this.id = id;
     }
 
-    public int getAnswer() {
+    public int[] getAnswer() {
         return answer;
     }
 
-    public void setAnswer(int answer) {
+    public void setAnswer(int[] answer) {
         this.answer = answer;
     }
 
