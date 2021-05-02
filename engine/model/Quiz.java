0efeb44ce66eaf239345c7c866f367f665005590
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-02 10:56:30 +0100

    created ManyToOne relationship with User

--- engine/model/Quiz.java
+++ engine/model/Quiz.java
@@ -2,15 +2,11 @@ package engine.model;
 
 import org.springframework.validation.annotation.Validated;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Validated
@@ -29,6 +25,9 @@ public class Quiz {
     private List<String> options;
     @ElementCollection
     private List<Integer> answer;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     public Quiz() {
     }
