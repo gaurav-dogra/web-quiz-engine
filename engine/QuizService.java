56778bae8d64763d6db66705abdebadc8ad3c8d9
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-02 09:23:08 +0100

    QuizRepository is autowired, and save method now saves the parametr quiz in persistent database

--- engine/QuizService.java
+++ engine/QuizService.java
@@ -1,6 +1,7 @@
 package engine;
 
 import engine.model.Quiz;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -9,12 +10,17 @@ import java.util.stream.Collectors;
 @Service
 public class QuizService {
     private final Map<Long, Quiz> quizzes = new HashMap<>();
+    private QuizRepository quizRepository;
+
+    @Autowired
+    public QuizService(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
 
     public ServerResponseQuiz save(Quiz quiz) {
-        long id = quizzes.size() + 1;
-        quiz.setId(id);
-        quizzes.put(id, quiz);
-        return ServerResponseQuiz.valueOf(quiz);
+        Quiz quizDb = quizRepository.save(quiz);
+        System.out.println("quizDb = " + quizDb);
+        return ServerResponseQuiz.valueOf(quizDb);
     }
 
     public ServerResponseQuiz getQuizById(long id) {
@@ -34,23 +40,23 @@ public class QuizService {
         return quiz != null;
     }
 
-    public Reply solve(long id, int[] parameterAnswer) {
-        Quiz quiz = quizzes.get(id);
-        int[] databaseAnswer = quiz.getAnswer();
-
-        Set<Integer> databaseAnswerSet = Arrays.stream(databaseAnswer)
-                    .boxed()
-                    .collect(Collectors.toSet());
-
-        Set<Integer> parameterAnswerSet = Arrays.stream(parameterAnswer)
-                    .boxed()
-                    .collect(Collectors.toSet());
-
-        if (databaseAnswerSet.equals(parameterAnswerSet)) {
-            return Reply.CORRECT_ANSWER;
-        } else {
-            return Reply.INCORRECT_ANSWER;
-        }
-    }
+//    public Reply solve(long id, int[] parameterAnswer) {
+//        Quiz quiz = quizzes.get(id);
+//        List<Integer> databaseAnswer = quiz.getAnswer();
+//
+//        Set<Integer> databaseAnswerSet = Arrays.stream(databaseAnswer)
+//                    .boxed()
+//                    .collect(Collectors.toSet());
+//
+//        Set<Integer> parameterAnswerSet = Arrays.stream(parameterAnswer)
+//                    .boxed()
+//                    .collect(Collectors.toSet());
+//
+//        if (databaseAnswerSet.equals(parameterAnswerSet)) {
+//            return Reply.CORRECT_ANSWER;
+//        } else {
+//            return Reply.INCORRECT_ANSWER;
+//        }
+//    }
 }
 
