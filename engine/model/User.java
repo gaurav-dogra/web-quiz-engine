0f6ffc9c5a9ce87c58a4e5ab139f105296b3b09d
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-02 10:55:29 +0100

    Added User class, @Entity and OneToMany relationshipt with Quiz

---
+++ engine/model/User.java
@@ -0,0 +1,60 @@
+package engine.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    private long id;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user")
+    private List<Quiz> quizzes;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Quiz> getQuizzes() {
+        return quizzes;
+    }
+
+    public void setQuizzes(List<Quiz> quizzes) {
+        this.quizzes = quizzes;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
