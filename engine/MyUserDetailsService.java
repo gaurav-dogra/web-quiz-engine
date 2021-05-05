d3136fef2b463a020d9f28e96f8bb5baebeb20ad
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-05 21:52:41 +0100

    added MyUserDetailsService implementing UserDetailsService

---
+++ engine/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package engine;
+
+import engine.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            return new MyUserPrincipal(user.get());
+        } else {
+            throw new UsernameNotFoundException(email);
+        }
+    }
+}
