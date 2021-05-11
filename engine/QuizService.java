59d17011adc3e9a9ce2bdf250552abadbe9fdc2c
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-11 22:58:28 +0100

    added paging and sorting capabilities in QuizService.getAll

--- engine/QuizService.java
+++ engine/QuizService.java
@@ -3,6 +3,10 @@ package engine;
 import engine.model.Quiz;
 import engine.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -28,11 +32,17 @@ public class QuizService {
         return ServerResponseQuiz.valueOf(quizDb);
     }
 
-    public List<ServerResponseQuiz> getAll() {
-        return quizRepository.findAll()
-                .stream()
-                .map(ServerResponseQuiz::valueOf)
-                .collect(Collectors.toList());
+    public List<ServerResponseQuiz> getAll(Integer pageNo, Integer pageSize, String sortBy) {
+        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Page<Quiz> pageResult = quizRepository.findAll(paging);
+        if (pageResult.hasContent()) {
+            return pageResult.getContent()
+                    .stream()
+                    .map(ServerResponseQuiz::valueOf)
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<ServerResponseQuiz>();
+        }
     }
 
     public boolean quizExist(long id) {
